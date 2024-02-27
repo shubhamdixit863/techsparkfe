2284ded1b59f73c79a98de499c658740f3d91062
MD SHAMIM AKHTER <contactshamim62@gmail.com>
2024-02-27 14:19:18 +0530

    margin added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,7 +10,7 @@ const About = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="w-10/12 md:w-8/12 lg:w-6/12 text-white p-4">
+      <div className="w-10/12 md:w-8/12 lg:w-6/12 text-white p-4 md:mt-8">
         <h1 className="text-3xl font-bold mb-4 text-white">About TechSpark</h1>
         <p className="text-white-600 mb-6">
           Welcome to TechSpark, your gateway to high-quality IT training. At TechSpark, we pride ourselves on delivering cutting-edge courses facilitated by industry experts currently working in top product-based companies.
